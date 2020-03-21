46e21ddf0ba5b8c28edef945c4e4187f90a863e8
Martin Beierling-Mutz <mbeierling@gmail.com>
2020-03-21 10:03:38 +0100

    Setup graphql & add initial mock schema

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,51 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import ApolloClient from "apollo-boost";
+import { gql } from "apollo-boost";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+
+const GRAPHQL_URL = process.env.GRAPHQL_URL || "http://localhost:9002/graphql";
+
+const client = new ApolloClient({
+  uri: GRAPHQL_URL
+});
+
+// Temporary test
+client
+  .query({
+    query: gql`
+      {
+        hospital(id: "test") {
+          id
+          name
+          address {
+            zipCode
+            street
+            latitude
+            longitude
+          }
+          email
+          website
+          phone
+          personnelRequests {
+            matches {
+              helper {
+                name
+                email
+                phone
+              }
+            }
+          }
+        }
+      }
+    `
+  })
+  .then(result => console.log(result));
+
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
