313fc786c1ea20b49dfce692da64aa361113acb9
Davis Kirkendall <davis.e.kirkendall@gmail.com>
2020-03-21 15:56:39 +0100

    Use db url instead of single components

--- backend/wirvsvirus/settings.py
+++ backend/wirvsvirus/settings.py
@@ -5,7 +5,7 @@ class Settings(BaseSettings):
 
     host: str = "127.0.0.1"
     db_db: str = "default"
-    db_url: str = "mongodb://root:pass@db:27017/default"
+    db_url: str = "mongodb://root:pass@127.0.0.1:27017/default"
 
     class Config:
         env_prefix = "wirvsvirus_"
