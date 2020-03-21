22580fc7e6d7d98405de10b74e7c985766ae1319
Martin Beierling-Mutz <mbeierling@gmail.com>
2020-03-21 10:09:32 +0100

    Setup apollo client for react & add test query

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import ApolloClient from "apollo-boost";
 import { gql } from "apollo-boost";
+import { ApolloProvider } from "@apollo/react-hooks";
 
 import "./index.css";
 import App from "./App";
@@ -13,39 +14,12 @@ const client = new ApolloClient({
   uri: GRAPHQL_URL
 });
 
-// Temporary test
-client
-  .query({
-    query: gql`
-      {
-        hospital(id: "test") {
-          id
-          name
-          address {
-            zipCode
-            street
-            latitude
-            longitude
-          }
-          email
-          website
-          phone
-          personnelRequests {
-            matches {
-              helper {
-                name
-                email
-                phone
-              }
-            }
-          }
-        }
-      }
-    `
-  })
-  .then(result => console.log(result));
-
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
