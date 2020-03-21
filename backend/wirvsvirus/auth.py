fbe3fb0e57ce5d38a0daec29bcf2edf8b7a814b7
Davis Kirkendall <davis.e.kirkendall@gmail.com>
2020-03-21 16:47:07 +0100

    Add initial backend authentication utilities

---
+++ backend/wirvsvirus/auth.py
@@ -0,0 +1,74 @@
+# Authorization utilities
+
+import functools
+from typing import Any, Dict, List, Optional
+
+import requests
+from authlib.jose import JWTClaims, jwt
+from authlib.jose.errors import JoseError
+from fastapi import FastAPI, HTTPException
+from fastapi.security import HTTPBearer
+from pydantic import BaseModel
+from starlette.requests import Request
+from starlette.status import HTTP_401_UNAUTHORIZED
+
+from wirvsvirus.settings import settings
+
+JWK = Dict[str, Any]
+
+
+class JWKS(BaseModel):
+    keys: List[JWK] = []
+
+
+class JWTAuthorizationCredentials(BaseModel):
+    jwt_token: str
+    header: Dict[str, str]
+    claims: Dict[str, str]
+    signature: str
+    message: str
+
+
+@functools.lru_cache()
+def get_jwks() -> JWKS:
+    """Get JSON Web Key Set from auth settings.
+
+    More information: https://auth0.com/docs/tokens/concepts/jwks
+    """
+    response = requests.get(settings.auth_jwks_uri, timeout=10)
+    response.raise_for_status()
+    response_json = response.json()
+    return JWKS(**response_json)
+
+
+class Auth(HTTPBearer):
+    """Authorization header jwt bearer handler.
+
+    This can be used as a dependency and will verify that an http bearer token
+    is signed by the correct authority.
+    """
+
+    @functools.cached_property
+    def jwks(self) -> JWKS:
+        """Get json web key set."""
+        return get_jwks()
+
+    @functools.cached_property
+    def kid_to_jwk(self) -> Dict[str, JWK]:
+        """Map key ids to json web key."""
+        return {jwk["kid"]: jwk for jwk in self.jwks.keys if jwk.get("kid")}
+
+    def decode_jwt(self, token: str) -> JWTClaims:
+        """Verify jwt."""
+        try:
+            payload = jwt.decode(token, self.jwks.keys, claims_options={"iss": {"value": settings.auth_issuer}})
+            payload.validate()
+        except JoseError as e:
+            raise HTTPException(status_code=HTTP_401_UNAUTHORIZED, detail=e.args[0])
+
+        return payload
+
+    async def __call__(self, request: Request) -> Optional[dict]:
+        token = await super().__call__(request)
+        return self.decode_jwt(token)
+
--- backend/wirvsvirus/settings.py
+++ backend/wirvsvirus/settings.py
@@ -2,10 +2,22 @@ from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
+    """General settings.
 
+    Can be overriden by environment variables: i.e. host can be overridden by
+    WIRSINDVIRUS_HOST.
+
+    """
+    # api settings
     host: str = "127.0.0.1"
+
+    # database settings
     db_db: str = "default"
     db_url: str = "mongodb://root:pass@127.0.0.1:27017/default"
+
+    # authentication settings
+    auth_issuer: str = "https://dev-healthkeeper.eu.auth0.com/"
+    ath_jwk_uri: str = "https://dev-healthkeeper.eu.auth0.com/.well-known/jwks.json"
 
     class Config:
         env_prefix = "wirvsvirus_"
