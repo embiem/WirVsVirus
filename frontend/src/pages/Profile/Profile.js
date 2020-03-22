41cfce0eff8336fa15554491cc9bfe59908ba4c2
Stefan Adolf <stefan.adolf@turbinekreuzberg.com>
2020-03-22 22:05:17 +0100

    getting current proifile type from api proceeding from profile

--- frontend/src/pages/Profile/Profile.js
+++ frontend/src/pages/Profile/Profile.js
@@ -1,20 +1,51 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   Container, Button, Paper, Box, Avatar,
 } from '@material-ui/core';
 import { Link as RouterLink } from '@reach/router';
+import axios from 'axios';
 import { useAuth0 } from '../../utils/react-auth0-spa';
 import useLoginRedirect from '../../utils/useLoginRedirect';
 
-const Profile = () => {
-  // Will redirect to login if not already authenticated
-  useLoginRedirect();
-
-  const { loading, user } = useAuth0();
-
-  if (loading || !user) {
-    return <div>Loading...</div>;
+const Proceed = ({ profileType }) => {
+  if (profileType === 'helper') {
+    return <div>
+        Du bist ein Helfer.
+        <RouterLink to="/register">Hier</RouterLink> kannst Du Dein Profil verändern
+      </div>;
+  } if (profileType === 'hospital') {
+    return <div>
+        Du suchst als Krankenhausmitarbeiter nach Hilfe.
+        Suche <RouterLink to="/hospital">hier</RouterLink> nach Helfern.
+      </div>;
   }
+  return '';
+};
+const Profile = () => {
+  useLoginRedirect();
+  const { user } = useAuth0();
+
+  const [profileType, setProfileType] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+    // todo: this should be a custom hook.
+      const Axios = axios.create({
+        baseURL: process.env.REACT_APP_API_URL || 'http://localhost:8000',
+        headers: {
+          Authorization: document.auth_token ? `Bearer ${document.auth_token}` : '',
+        },
+      });
+      try {
+        const res = await Axios.get('/profile');
+        console.log(res);
+        setProfileType(res.data.profileType);
+      } catch (e) {
+        console.log(e);
+      }
+    })();
+  });
+
 
   return (
         <Container >
@@ -25,7 +56,10 @@ const Profile = () => {
             </Box>
               <h2>{user.name}</h2>
               <p>{user.email}</p>
-               <Button to="/register" component={RouterLink}>registrieren</Button>
+              {profileType}
+              { !profileType ? <Button to="/register" component={RouterLink}>registrieren</Button>
+                : <Proceed profileType={profileType} />
+  }
           </Box>
         </Paper>
      </Container>
